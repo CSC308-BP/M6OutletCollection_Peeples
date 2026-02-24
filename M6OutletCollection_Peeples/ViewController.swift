179299dc7ac3d1145ea61dae0c356b6408fea3ac
Brayden Peeples <braydenpeeples@gmail.com>
2026-02-24 11:49:37 -0500

    Initial Commit

---
+++ M6OutletCollection_Peeples/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  M6OutletCollection_Peeples
+//
+//  Created by Brayden Peeples on 2/24/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
