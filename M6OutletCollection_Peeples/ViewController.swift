cb7b15fe57bae92db20583cc9c906aa4cf6b46dd
Brayden Peeples <braydenpeeples@gmail.com>
2026-02-24 12:20:41 -0500

    Done, I think

--- M6OutletCollection_Peeples/ViewController.swift
+++ M6OutletCollection_Peeples/ViewController.swift
@@ -9,9 +9,59 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let titles = ["First", "Second", "Third", "Fourth"]
+    
+    @IBOutlet var testButtons: [UIButton]!
+    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet var actionButtons: [UIButton]!
+    
+    var count = 0
+    @IBAction func actionButtonTapped(_ sender: UIButton) {
+        count += sender.tag
+        countLabel.text = "\(count)"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        for button in testButtons {
+            button.backgroundColor = .orange
+            button.setTitleColor(.black, for: .normal)
+            button.layer.cornerRadius = 10
+            
+            if button.tag < titles.count {
+                button.setTitle(titles[button.tag], for: .normal)
+            }
+            
+        }
+        
+        if let buttons = actionButtons {
+            buttons[0].tag = -1
+            buttons[1].tag = 1
+            buttons[0].setTitle("-", for: .normal)
+            buttons[1].setTitle("+", for: .normal)
+        }
+        countLabel.text = "\(count)"
+        
+    }
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        print("Tapped Tag: \(sender.tag)")
+        
+        guard sender.tag < titles.count else { return }
+        
+        let originalTitle = titles[sender.tag]
+        let currentTitle = sender.title(for: .normal)
+        
+        if currentTitle == originalTitle {
+            sender.setTitle("Checked", for: .normal)
+            sender.setTitleColor(.yellow, for: .normal)
+        }
+        else {
+            sender.setTitle(originalTitle, for: .normal)
+            sender.setTitleColor(.black, for: .normal)
+        }
+        
     }
 
 
